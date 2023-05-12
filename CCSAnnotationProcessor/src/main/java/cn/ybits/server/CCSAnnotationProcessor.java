1be5ae674117d9759c98ee8b07e10eefb780c878
Dong, Martin <djpmp2010@hotmail.com>
2023-05-12 08:24:08 +0800

    Update modules.

--- CCSAnnotationProcessor/src/main/java/cn/ybits/server/CCSAnnotationProcessor.java
+++ CCSAnnotationProcessor/src/main/java/cn/ybits/server/CCSAnnotationProcessor.java
@@ -10,22 +10,9 @@ import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
-
-@SupportedAnnotationTypes( {"cn.ybits.server.annotation.ResponseBody"})
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class CCSAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        // ResponseBody
-        Messager messager = processingEnv.getMessager();
-        for (TypeElement typeElement : annotations) {
-            System.out.println("getQualifiedName--" + typeElement.getQualifiedName());
-            System.out.println("Kind is: " + typeElement.getKind().toString());
-            if (typeElement.getKind() == ElementKind.ANNOTATION_TYPE) {
-                System.out.println("METHOD:Annotation Invoking.");
-                messager.printMessage(Diagnostic.Kind.NOTE, "printMessage:" + typeElement.toString());
-            }
-        }
         return false;
     }
 }
---
+++ CCSAnnotationProcessor/src/main/java/cn/ybits/server/CCSResponseBodyAnnotationProcessor.java
@@ -0,0 +1,28 @@
+package cn.ybits.server;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes( {"cn.ybits.server.annotation.ResponseBody"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class CCSResponseBodyAnnotationProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        // ResponseBody
+        Messager messager = processingEnv.getMessager();
+        for (TypeElement typeElement : annotations) {
+            System.out.println("getQualifiedName--" + typeElement.getQualifiedName());
+            System.out.println("Kind is: " + typeElement.getKind().toString());
+            if (typeElement.getKind() == ElementKind.ANNOTATION_TYPE) {
+                System.out.println("METHOD:Annotation Invoking.");
+                messager.printMessage(Diagnostic.Kind.NOTE, "printMessage:" + typeElement.toString());
+            }
+        }
+        return false;
+    }
+}
