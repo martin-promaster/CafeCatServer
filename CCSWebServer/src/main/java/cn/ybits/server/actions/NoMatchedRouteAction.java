144d30a881c6c9ec5395408e90af56d06cae86a9
Dong, Martin <djpmp2010@hotmail.com>
2023-05-21 23:25:38 +0800

    Fix issue: warning of try-resouce when using FileInputStream class.

--- CCSWebServer/src/main/java/cn/ybits/server/actions/NoMatchedRouteAction.java
+++ CCSWebServer/src/main/java/cn/ybits/server/actions/NoMatchedRouteAction.java
@@ -8,32 +8,27 @@ import java.io.*;
 
 public class NoMatchedRouteAction extends AbsRouteAction implements IService {
 
-
     public void doAction(HttpRequest request, HttpResponse response) {
 
         String WEB_APP_PATH = WEB_ROOT + WEB_APP;
 
-        FileInputStream in;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        byte[] buf = new byte[1024];
-        try {
-            in = new FileInputStream(WEB_APP_PATH+request.getPath());
 
-            int rt = -1;
-            while((rt = in.read(buf)) > 0) {
-                out.write(buf, 0, rt);
+        try {
+            try (FileInputStream in = new FileInputStream(WEB_APP_PATH+request.getPath())) {
+                byte[] buf = new byte[1024];
+                int rt = -1;
+                while((rt = in.read(buf)) > 0) {
+                    out.write(buf, 0, rt);
+                }
             }
             response.setPayload(out.toByteArray());
-            
         } catch (FileNotFoundException e) {
             response.setNotFoundMessage();
             //e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 }
