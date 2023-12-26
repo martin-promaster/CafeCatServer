f538f51bc92966181cf162de1b52fbde72dbf911
Martin Dong <martin.dong@139.com>
2023-12-26 15:38:27 +0800

    Update version of log4j

--- ccsstart.sh
+++ ccsstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 M2_REPO="~/.m2/repository"
-M2_JARS="${M2_REPO}/org/apache/logging/log4j/log4j-core/2.20.0/log4j-core-2.20.0.jar:${M2_REPO}/org/apache/logging/log4j/log4j-api/2.20.0/log4j-api-2.20.0.jar"
+M2_JARS="${M2_REPO}/org/apache/logging/log4j/log4j-core/2.22.0/log4j-core-2.22.0.jar:${M2_REPO}/org/apache/logging/log4j/log4j-api/2.22.0/log4j-api-2.22.0.jar"
 CCS_OUTPUT="./out"
 CCS_JARS="${CCS_OUTPUT}/CCSAnnotationProcessor-1.0-SNAPSHOT.jar:${CCS_OUTPUT}/CCSDataService-1.0-SNAPSHOT.jar:${CCS_OUTPUT}/CCSWebServer-1.0-SNAPSHOT.jar"
 echo "CLASSPATH is:" ${CCS_JARS}:${M2_JARS}
